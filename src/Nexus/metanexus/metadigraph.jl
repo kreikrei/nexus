2eb7ddb3294fbb21a03ee2095af38319fd426f31
kreiton <kreitonstrs@gmail.com>
2021-12-10 15:30:46 +0700

    format metadigraph

--- src/Nexus/metanexus/metadigraph.jl
+++ src/Nexus/metanexus/metadigraph.jl
@@ -1,20 +1,21 @@
 mutable struct MetaDigraph{T} <: AbstractMetaNexus{T}
     core::Digraph{T}
-    nprops::Dict{T, PropDict}
-    aprops::Dict{Arc{T}, PropDict}
+    nprops::Dict{T,PropDict}
+    aprops::Dict{Arc{T},PropDict}
 end
 
-MetaDigraph(core::Digraph{T}) where T = MetaDigraph{T}(core, Dict{T, PropDict}(), Dict{Arc{T}, PropDict}())
-MetaDigraph{T}() where T = MetaDigraph(Digraph{T}())
+MetaDigraph(core::Digraph{T}) where {T} =
+    MetaDigraph{T}(core, Dict{T,PropDict}(), Dict{Arc{T},PropDict}())
+MetaDigraph{T}() where {T} = MetaDigraph(Digraph{T}())
 
 is_directed(::MetaDigraph) = true
 
 @forward MetaDigraph.core badj
 props(g::MetaDigraph, a::Arc) = get(PropDict, g.aprops, a)
 
-Digraph(g::MetaDigraph{T}) = g.core
+Digraph(g::MetaDigraph{T}) where {T} = g.core
 
-function set_props!(g::MetaDigraph{T}, a::Arc{T}, d::PropDict)
+function set_props!(g::MetaDigraph{T}, a::Arc{T}, d::Dict) where {T}
     if has_arc(g, a)
         !_hasdict(g, a) ? g.aprops[a] = d : merge!(g.aprops[a], d)
         return true
@@ -23,5 +24,5 @@ function set_props!(g::MetaDigraph{T}, a::Arc{T}, d::PropDict)
     end
 end
 
-rem_prop!(g::MetaDigraph{T}, a::Arc{T}, prop::Symbol) where T =
+rem_prop!(g::MetaDigraph{T}, a::Arc{T}, prop::Symbol) where {T} =
     delete!(g.aprops[a], prop)
