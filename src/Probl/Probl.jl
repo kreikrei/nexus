6e464370fe9f57e6288d28af397e0db7f2d51c06
kreikrei <kreitonstrs@gmail.com>
2021-12-13 13:07:14 +0700

    remove todo #7

--- src/Probl/Probl.jl
+++ src/Probl/Probl.jl
@@ -22,7 +22,6 @@ permintaan = readdata("/data/permintaan-recreated.csv")
 demandlist = demands(permintaan)
 demandscenarios = generatedemands(demandlist, 200, 1000)
 
-# TODO #7 uncertain demand generator able to be sampled
 # TODO #2 function to transform base network into expanded network
 # TODO #3 function to build deterministic model from expanded network
 # TODO #4 model to generate demand sample
