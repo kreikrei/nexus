3fb0ba7c2596c29f4b6635e0c61318e3107be318
kreikrei <kreitonstrs@gmail.com>
2021-12-13 13:34:55 +0700

    bingung

--- src/Probl/Probl.jl
+++ src/Probl/Probl.jl
@@ -32,6 +32,8 @@ T_max,t_max_idx = findmax([lp.per for lp in keys(demandlist)])
 #    b. transportation arcs
 # 3. ensure zero sum demand
 
+# tes lagi dulu deh
+
 # TODO #2 function to transform base network into expanded network
 # TODO #3 function to build deterministic model from expanded network
 # TODO #4 model to generate demand sample
